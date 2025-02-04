bfff79832bf1f7624f77d30bfd7f1f05e33b47ae
rizanurmaulana <rizanurmaulana2810@gmail.com>
2025-02-04 17:38:00 +0700

    feat: initial project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <h1 className='text-5xl font-semibold'>Hello Selaras!</h1>
+    </div>
+  );
+}
+
+export default App;
